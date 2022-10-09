a1e6f21b85d9b531759af6512564c15b48e13520
S7v7N <lmxcpp100@gmail.com>
2022-10-09 07:19:50 -0400

    Add main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,51 @@
-#include <iostream>
+#include <iostream> // prinf, scanf, puts, NULL 
+#include <stdlib.h> // srand, rand 
+#include <time.h> // time 
+#include <string>
 
 int main() {
-	std::cout << "FUCK" << std::endl;
+	const std::string hotelName = "Dracula | The Beginning";
+	const int totalRooms = 134;
+	const int firstRoomNumber = 110;
+
+	
+	// generate a number between 0 and 19
+	srand(time(NULL));
+
+	int roomsOccupied = rand() % totalRooms + 1;
+	int roomsAvailable = totalRooms - roomsOccupied;
+
+	float occupancyRate = ( float (roomsOccupied) / float (totalRooms)) * 100;
+	std::string occupancyLevel;
+
+	if (occupancyRate > 70) {
+		occupancyLevel = "High";
+
+	} else if (occupancyRate > 20) {
+		occupancyLevel = "Medium";
+	} else {
+		occupancyLevel = "Low";
+	}
+
+	std::cout << "Hotel: " << hotelName << "\n";
+	std::cout << "Number of rooms: " << totalRooms << "\n";
+	std::cout << "Rooms available: " << roomsAvailable << "\n";
+	std::cout << "					  OccupancyLevel: " << occupancyLevel << "\n";
+	printf("				          OccupancyRate: %0.2f %%\n", occupancyRate);
+
+	if (roomsAvailable > 0) {
+		std::cout << "Rooms: \n";
+		for (int i = 0; roomsAvailable > i; i++) {
+			int roomNumber = firstRoomNumber + i;
+			int size = rand() % 6 + 1;
+			int nights = rand() % 10 + 1;
+			std::cout << roomNumber << ":" << size << " people / " << nights << " nights" << "\n";
+		} 
+	
+	} else {
+		std::cout << "N.R.A.T \n";
+	}
+
+
 	return 0;
 }
